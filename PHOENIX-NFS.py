fc1d5db23cce1d40b7dc4bcde1d53e2168a41a78
Vishwadeep2026 <vishwadeep2026@gmail.com>
2025-11-16 08:24:25 -0800

    Update PHOENIX-NFS.py

--- PHOENIX-NFS.py
+++ PHOENIX-NFS.py
@@ -220,4 +220,7 @@ def home():
     return render_template_string(HTML, message=message)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
